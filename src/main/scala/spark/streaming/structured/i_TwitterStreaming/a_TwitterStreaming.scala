43bbde6fe4e4801aa867f1eaab5f2a0b823998e2
prudhviakella <prudhvi.akella@abylle.in>
2019-11-28 16:46:28 +0530

    SparkStreaming Word Count1

--- src/main/scala/spark/streaming/structured/i_TwitterStreaming/a_TwitterStreaming.scala
+++ src/main/scala/spark/streaming/structured/i_TwitterStreaming/a_TwitterStreaming.scala
@@ -5,23 +5,20 @@ package spark.streaming.structured.i_TwitterStreaming
  * Desc:Twitter Streams output to Socket
  */
 
-import java.io.ObjectOutputStream
+import java.io.{PrintWriter}
 import java.net.{ServerSocket, Socket}
-
 import twitter4j.{FilterQuery, StallWarning, Status, StatusDeletionNotice, StatusListener, TwitterStream, TwitterStreamFactory}
 import twitter4j.conf.ConfigurationBuilder
 
-object a_TwitterStreaming extends App{
+object TwitterStreaming extends App{
   //Creating ServerSocket Connection
   val server = new ServerSocket(9999)
   println("Waiting for the client request")
   //Accept the connections from client
-  //It waits here until connection.Once the connection is established then only remaining code
+  //It waits for new connection.Once the connection is established then only remaining code
   //will execute.
   val socket = server.accept
   System.out.println("Connected");
-  //Creating ObjectOutputStream to write the twitter messages to socket
-  val oos = new ObjectOutputStream(socket.getOutputStream)
   //Creating ConfigurationBuilder which hold the consumerKey,Secret,AccessToken,Key of twitter account
   //which will be used for authentication and authorization
   val cb = new ConfigurationBuilder();
@@ -38,24 +35,28 @@ object a_TwitterStreaming extends App{
     //that will be tracked through status.
     def onStatus(status:Status) {
       println("@" + status.getUser().getScreenName() + " - " + status.getText());
-      //write object to Socket
-      oos.writeObject(status.getText())
+      val outputStream = socket.getOutputStream
+      //Creating PrintWriter to write the twitter messages to socket where spark is listing
+      val out = new PrintWriter(outputStream, true)
+      //write message to Socket
+      out.println(status.getText())
+      out.flush
     }
     def onDeletionNotice( statusDeletionNotice:StatusDeletionNotice) {
       println("Got a status deletion notice id:" + statusDeletionNotice.getStatusId());
     }
 
-    def onTrackLimitationNotice( numberOfLimitedStatuses:Int) {
+   def onTrackLimitationNotice( numberOfLimitedStatuses:Int) {
       println("Got track limitation notice:" + numberOfLimitedStatuses);
     }
-    def onScrubGeo( userId:Long,  upToStatusId:Long) {
+   def onScrubGeo( userId:Long,  upToStatusId:Long) {
       println("Got scrub_geo event userId:" + userId + " upToStatusId:" + upToStatusId);
     }
 
     def onStallWarning( warning:StallWarning) {
       println("Got stall warning:" + warning);
     }
-    def onException( ex:Exception) {
+   def onException( ex:Exception) {
       ex.printStackTrace();
     }
   };
@@ -65,4 +66,5 @@ object a_TwitterStreaming extends App{
   //twitterStream.filter(new FilterQuery().follow(1344951).follow(5988062))
   //Use this if you want to track a keywork.
   twitterStream.filter(new FilterQuery().track("fifa","nba","ipl"))
-}
+
+  }
